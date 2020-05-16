b540f44914cf04bf42d21250c6f3d5eadc9aae4f
geon <gchshin@gmail.com>
2020-05-16 18:40:58 +0900

    My Django first blog app, first commit

---
+++ blog/views.py
@@ -0,0 +1,80 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+from .models import Post
+from .forms import PostForm
+
+# Create your views here.
+def post_list(request):
+    qs = Post.objects.all()
+    qs = qs.filter(published_date__lte=timezone.now())
+    qs = qs.order_by('published_date')
+
+    return render(request, 'blog/post_list.html', {
+        'post_list': qs,
+    })
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {
+        'post': post,
+    })
+
+
+def post_new(request):
+
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            # post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', post.pk)
+    else:
+        form = PostForm()
+
+    return render(request, 'blog/post_edit.html', {
+        'form': form,
+    })
+
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            # post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', post.pk)
+    else:
+        form = PostForm(instance=post)
+
+    return render(request, 'blog/post_edit.html', {
+        'form': form,
+    })
+
+
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('post_list')
+
+
+def post_draft_list(request):
+    qs = Post.objects.all()
+    qs = qs.filter(published_date__isnull=True)
+    qs = qs.order_by('created_date')
+    
+    return render(request, 'blog/post_draft_list.html', {
+        'post_list': qs,
+    })
+
+
+def post_publish(requset, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('post_detail', post.pk)
